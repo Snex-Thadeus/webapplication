499e29f7ad268789769e97da79a483866d78220d
Snex-Thadeus <lwande94@gmail.com>
2020-08-30 12:18:46 +0300

    Employees record web app

--- webapplication/settings.py
+++ webapplication/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+p)weoy6_a7&u26hw5r+-!9-c$10a$!vut2@t5i9dc1z7^c@2x'
+SECRET_KEY = '****'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
